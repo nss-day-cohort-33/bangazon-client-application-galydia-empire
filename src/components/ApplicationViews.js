810e99c8b9aed4349b5b7220606f84a608e07720
scottsilver <scottsilver47@gmail.com>
2019-10-09 10:56:12 -0500

    commit for new push

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Route, withRouter, Redirect } from "react-router-dom";
 import React from "react";
 import useSimpleAuth from "../hooks/ui/useSimpleAuth";
@@ -8,7 +7,7 @@ import ProductCategories from "./productcategories/ProductCategories";
 import HomeProduct from "./home/HomeProduct";
 import ProductDetails from "./product/ProductDetails";
 import Profile from "./profile/Profile";
-import PaymentTypeForm from "./paymenttype/PaymentTypeForm"
+import PaymentTypeForm from "./paymenttype/PaymentTypeForm";
 
 const ApplicationViews = () => {
   const { isAuthenticated } = useSimpleAuth();
@@ -59,6 +58,13 @@ const ApplicationViews = () => {
       />
 
       <Route
+        path="/sell-product"
+        render={props => {
+          return <SellProduct {...props} />;
+        }}
+      />
+
+      <Route
         path="/productcategories"
         render={props => {
           return <ProductCategories {...props} />;
@@ -78,63 +84,3 @@ const ApplicationViews = () => {
 };
 
 export default withRouter(ApplicationViews);
-=======
-import { Route, withRouter } from "react-router-dom"
-import React from "react"
-import Register from "./auth/Register"
-import Login from "./auth/Login"
-import SellProduct from "./home/SellProduct"
-import ProductCategories from "./productcategories/ProductCategories"
-import HomeProduct from "./home/HomeProduct"
-import ProductDetails from "./product/ProductDetails"
-
-const ApplicationViews = () => {
-    return (
-        <React.Fragment>
-            <Route
-                exact path="/" render={props => {
-                    return (
-                    <>
-                    <HomeProduct {...props} />
-                    </>
-                    )
-                }}
-            />
-
-            <Route
-                path="/register" render={props => {
-                    return <Register {...props} />
-                }}
-            />
-
-            <Route
-                path="/login" render={props => {
-                    return <Login {...props} />
-                }}
-            />
-            <Route
-                path="/sell-product" render={props => {
-                    return <SellProduct {...props} />
-                }}
-            />
-
-
-
-            <Route
-                path="/productcategories" render={props => {
-                    return <ProductCategories {...props} />
-                }}
-                />
-
-            <Route
-                exact path="/product/:productId(\d+)" render={props => {
-                    let productId = +props.match.params.productId
-                    return <ProductDetails {...props} productId={productId} />
-                    }}
-                />
-                </React.Fragment>
-    )
-}
-
-export default withRouter(ApplicationViews)
->>>>>>> master
