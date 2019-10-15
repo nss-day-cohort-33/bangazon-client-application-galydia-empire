11644e1112ad4f988a4dcf9201e12da3b72fe4f8
Samuel Birky <sbirky21@gmail.com>
2019-10-15 16:11:07 -0500

    my products working, can delete any product that is not in an order

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -12,6 +12,7 @@ import PaymentTypeForm from "./paymenttype/PaymentTypeForm";
 import CategoryView from "./category/CategoryView"
 import MySettings from "./settings/MySettings";
 import PaymentTypes from "./paymenttype/PaymentTypes";
+import ProductList from "./product/ProductList"
 
 const ApplicationViews = () => {
   const { isAuthenticated } = useSimpleAuth();
@@ -65,6 +66,15 @@ const ApplicationViews = () => {
         path="/paymenttypes"
         render={props => {
           if (isAuthenticated()) return <PaymentTypes />;
+          else return <Redirect to="/login" />;
+        }}
+      />
+
+        <Route
+        exact
+        path="/myproducts"
+        render={props => {
+          if (isAuthenticated()) return <ProductList {...props} />;
           else return <Redirect to="/login" />;
         }}
       />
